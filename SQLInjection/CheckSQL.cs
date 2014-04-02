364ecfe99aeec855491a2ed6a52d9e3c062299a6
陈思远 <csy@17u.cn>
2014-04-02 13:50:34 +0800

    初始版本

---
+++ SQLInjection/CheckSQL.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Text;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace SQLInjection
+{
+    public class CheckSQL
+    {
+       
+        public static void Check(HttpApplication application,string keywords)
+        {
+            HttpContext context = application.Context;
+            HttpRequest request = context.Request;
+
+            string sqlkeywords = "exec↓select↓drop↓alter↓exists↓union↓and↓or↓xor↓order↓mid↓asc↓execute↓xp_cmdshell↓insert↓update↓delete↓join↓declare↓char↓sp_oacreate↓wscript.shell↓xp_regwrite↓'↓;↓--";
+ 
+            if (!string.IsNullOrEmpty(keywords))
+            {
+                if(keywords.StartsWith("↓"))
+                {
+                    sqlkeywords += keywords;
+                }
+                else
+                {
+                    sqlkeywords += "↓" + keywords;
+                } 
+            }
+
+            //Stopwatch watch = new Stopwatch();
+            //watch.Start();
+            string[] sqlkeyword = sqlkeywords.Split('↓');
+            foreach (string keyword in sqlkeyword)
+            {
+                // -----------------------防 Post 注入-----------------------
+                if (request.Form!= null)
+                {
+                    for (int k = 0; k <request.Form.Count; k++)
+                    {
+                        string getsqlkey = request.Form.Keys[k];
+                        string formValue= request.Form[getsqlkey].ToLower();
+                        string getip = Find(request, keyword,getsqlkey, formValue, (int)CheckItem.form);
+                    }
+                }
+                // -----------------------防 GET 注入-----------------------
+                if (request.QueryString != null)
+                {
+                    for (int k = 0; k <request.QueryString.Count; k++)
+                    {
+                        string getsqlkey = request.QueryString.Keys[k];
+                        string queryValue=request.QueryString[getsqlkey].ToLower();
+                        string getip = Find(request, keyword, getsqlkey, queryValue, (int)CheckItem.query);
+                    }
+                }
+                // -----------------------防 Cookies 注入-----------------------
+                if (request.Cookies != null)
+                {
+                    for (int k = 0; k < request.Cookies.Count; k++)
+                    {
+                        string getsqlkey = request.Cookies.Keys[k];
+                        string cookieValue=request.Cookies[getsqlkey].Value.ToLower();
+                        string getip = Find(request, keyword,getsqlkey, cookieValue, (int)CheckItem.cookie);
+                    }
+                }
+            }
+            //watch.Stop();
+            //TimeSpan ts = watch.Elapsed;
+            //string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            //    ts.Hours, ts.Minutes, ts.Seconds,
+            //    ts.Milliseconds / 10);
+            //System.Web.HttpContext.Current.Response.Write(elapsedTime);
+            //System.Web.HttpContext.Current.Response.End();
+        }
+
+        private static string Find(HttpRequest request, string keyword,string key, string value,int type)
+        {
+            string optionType = "";
+            switch (type)
+            { 
+                case (int)CheckItem.form:
+                    optionType = "Form ";
+                    break;
+                case (int)CheckItem.query:
+                    optionType = "Query ";
+                    break;
+                case (int)CheckItem.cookie:
+                    optionType = "Cookie ";
+                    break;
+            }
+
+            string getip = "";
+            string requestvalues = Regex.Replace(value, "\\s+", " ");
+            string[] values = requestvalues.Split(' ');
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == keyword)
+                {
+                    HttpContext.Current.Response.Write("<script Language=JavaScript>alert('防注入程序提示您，请勿提交非法字符！');</" + "script>");
+                    HttpContext.Current.Response.Write("非法操作！您存在非法的sql注入" + "<br>");
+                    if (request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null)
+                    {
+                        getip = request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+                    }
+                    else
+                    {
+                        getip = request.ServerVariables["REMOTE_ADDR"];
+                    }
+                    HttpContext.Current.Response.Write("操 作 I P ：" + getip + "<br>");
+                    HttpContext.Current.Response.Write("操 作 时 间：" + DateTime.Now.ToString() + "<br>");
+                    HttpContext.Current.Response.Write("操 作 页 面：" + request.ServerVariables["URL"] + "<br>");
+                    HttpContext.Current.Response.Write("提 交 方 式："+optionType + "<br>");
+                    HttpContext.Current.Response.Write("提 交 参 数：" + key + "<br>");
+                    HttpContext.Current.Response.Write("提 交 数 据：" + value + "<br>");
+                    HttpContext.Current.Response.End();
+
+                }
+            }
+            return getip;
+        }
+
+        private enum CheckItem:int
+        { 
+            form=1,
+            query,
+            cookie            
+        }
+
+    }
+}
---
+++ SQLInjection/SQLInjection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Text;
+using System.Web;
+using System.Configuration;
+
+namespace SQLInjection
+{
+    public class SQLInjection : IHttpModule
+    {
+        public void Init(HttpApplication application)
+        {
+            application.BeginRequest += new EventHandler(application_BeginRequest);
+        }
+
+        void application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication application = (HttpApplication)sender;
+
+            string keywords = "";
+            bool enable =true;
+            try
+            {
+                if (ConfigurationManager.AppSettings.Count > 0)
+                {
+                    string enableStr = ConfigurationManager.AppSettings["SQLInjectionEnable"]; //是否启用
+                    if (enableStr!= null)
+                    {
+                        if (enableStr.ToLower() == "false" || enableStr.ToLower() == "0")
+                        {
+                            enable = false;
+                        }
+                    }
+
+                    keywords = ConfigurationManager.AppSettings["SQLInjection"]; //追加的过滤关键词
+                }
+
+                if (enable)
+                {
+                    CheckSQL.Check(application, keywords);
+                }
+            }
+            catch
+            { 
+            
+            } 
+        }
+
+        public void Dispose()
+        { 
+            
+        }
+
+
+    }
+}
